79b938feafa6ab43e97203c3859bf1fd0a3a3aac
Milan Vlaski <milan.vlaski321@gmail.com>
2024-04-25 22:36:45 +0200

    test passes for incrementing seconds in Rest

--- src/main/java/main/Rest.java
+++ src/main/java/main/Rest.java
@@ -18,4 +18,7 @@ public class Rest
 	public long seconds()
 	{ return seconds; }
 
+	public long incrementSeconds()
+	{ return --seconds; }
+
 }
--- src/test/java/main/TestTimer.java
+++ src/test/java/main/TestTimer.java
@@ -2,7 +2,9 @@ package main;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.Duration;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 import org.junit.jupiter.api.*;
 
@@ -15,7 +17,7 @@ public class TestTimer
 		Timer timer;
 
 		LocalTime anyTime = LocalTime.MIN;
-		
+
 		@BeforeEach
 		void setup()
 		{ timer = new Timer(); }
@@ -33,15 +35,23 @@ public class TestTimer
 			Work work = timer.start(anyTime);
 			assertThat(work.incrementSeconds()).isEqualTo(1);
 		}
-		
+
 		@Test
 		void RestLastsFiveSecondsAfterWorksForTwentyFive()
 		{
 			LocalTime twentyFiveSecLater = anyTime.plusSeconds(25);
-			
+
 			Work work = timer.start(anyTime);
 			Rest rest = work.rest(twentyFiveSecLater);
 			assertThat(rest.seconds()).isEqualTo(5);
+		}
+
+		@Test
+		void RestMovesSecondsDown()
+		{
+			Duration twentyFiveSeconds = Duration.of(25, ChronoUnit.SECONDS);
+			Rest rest = new Rest(anyTime, twentyFiveSeconds);
+			assertThat(rest.incrementSeconds()).isEqualTo(4);
 		}
 	}
 
