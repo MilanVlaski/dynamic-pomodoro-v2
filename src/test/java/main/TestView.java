8bf84803d492b2721c0082b53c86fa395b98b909
Milan Vlaski <milan.vlaski321@gmail.com>
2024-04-28 14:42:31 +0200

    small rename

--- src/test/java/main/TestView.java
+++ src/test/java/main/TestView.java
@@ -14,22 +14,16 @@ public class TestView
 	@Test
 	void Increments_Seconds_While_Working() throws SessionTooLong
 	{
-		ViewModel viewModel = new ViewModel(new View(), new Timer(), new SingleCounter());
+		var viewModel = new ViewModel(new View(), new Timer(), new SingleCounter());
 
 		viewModel.startWorking();
 		assertThat(viewModel.seconds).isEqualTo(1);
 	}
-//
-//	@Test
-//	void Shows_Correct_Time_When_Rests_After_Work() throws SessionTooLong
-//	{
-//		var timer = new Timer();
-//		Counter counter = new CountsSeconds(25);
-//		FakeView view = new FakeView(timer, counter);
-//		view.startWorking();
-//		view.startResting(); 
-//		
-//		assertThat(view.seconds).isEqualTo(5);
-//	}
+
+	@Test
+	void Rests_Then_Works() throws SessionTooLong
+	{
+		
+	}
 
 }
