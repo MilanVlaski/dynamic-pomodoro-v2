ed68bd1d16ce4e829bae76a82fa366d1a08ff71a
Milan Vlaski <milan.vlaski321@gmail.com>
2024-04-30 14:13:36 +0200

    formatting

--- src/main/java/model/SingleCounter.java
+++ src/main/java/model/SingleCounter.java
@@ -9,13 +9,14 @@ public class SingleCounter implements Counter
 	public void count(Work work)
 	{ work.incrementSeconds(); }
 
-	@Override
-	public void stop()
-	{ this.working = false; }
 
 	@Override
 	public void count(Rest rest)
 	{ rest.decrementSeconds(); }
+
+	@Override
+	public void stop()
+	{ this.working = false; }
 
 	@Override
 	public boolean isWorking()
